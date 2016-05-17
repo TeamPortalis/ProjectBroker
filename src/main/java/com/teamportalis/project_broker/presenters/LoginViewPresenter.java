05e1c8dac6b869678552276adfab0a6a1c7ea2da
Alexander Mandt <mcgamer0070@gmail.com>
2016-05-17 13:23:04 +0200

    Cleaned filthy repository

--- src/main/java/com/teamportalis/project_broker/presenters/LoginViewPresenter.java
+++
@@ -1,8 +0,0 @@
-package com.teamportalis.project_broker.presenters;
-
-/**
- * Created by Moritz Brandstätter on 15.05.2016.
- */
-public interface LoginViewPresenter {
-    void onLogin(String user, String password);
-}
