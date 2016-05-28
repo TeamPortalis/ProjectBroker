17580139e405175d5317d6e530aac3abc99b8495
Alexander Mandt <mcgamer0070@gmail.com>
2016-05-28 21:55:14 +0200

    Reorganized files.

---
+++ ProjectBroker/Models/MainViewViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectBroker.Models.DBModel;
+
+namespace ProjectBroker.Models
+{
+    public class MainViewViewModel
+    {
+
+        public MainViewViewModel() { }
+
+        public string ActiveUser { get; set; }
+        public IEnumerable<pr_project> Projects { get; set; }
+        
+
+
+
+    }
+}
