19310a1b7baaadafd7fc742cdf99563b02bf2ee1
Alex <Alex@ASUS>
2016-06-06 20:21:55 +0200

    Started improving Project Creation Wizard

---
+++ ProjectBroker/Models/ViewModels/NewProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectBroker.Models.DBModel;
+
+namespace ProjectBroker.Models.ViewModels
+{
+    public class NewProjectViewModel
+    {
+        
+        public IEnumerable<phs_projhostingenv> AllProjectHostingEnvs { get; set; }
+        public IEnumerable<pms_projmanagementenv> AllProjectManagementEnvs { get; set; }
+
+        public IEnumerable<tm_team> AllTeams { get; set; }
+    }
+}
